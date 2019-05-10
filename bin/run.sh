9c862325ca366aed8ca27bcbb16b2e8bcc989b4a
Toby Ferguson <toby@cloudera.com>
2019-05-10 16:30:24 -0700

    Corrected minor errors

--- bin/run.sh
+++ bin/run.sh
@@ -7,11 +7,13 @@ Expected 3 arguments, got $#
 CM_URL CM_username CM_password
 
 EOF
+fi
+
 URL=${1:?No URL to the Cloudera Manager was given}
 USER=${2:?No CM user name was given}
 PASSWORD=${3:?No password provided}
 
-HOST=$(echo ${URL#*://} | sed -n 's/\(.*\):*.*/\1/p')
+HOST=$(echo ${URL#*://} | sed -n 's/\([^:][^:]*\).*/\1/p')
 
 for p in configure_jdk.py create_kudu_1.py create_nifi.py create_solr.py
 do
